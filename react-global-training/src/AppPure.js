9d36f3fcab8a8241e84943c0a092df4da61f0003
Adam Pryka <adam_pryka@epam.com>
2018-04-12 11:49:25 +0200

    Task1

---
+++ react-global-training/src/AppPure.js
@@ -0,0 +1,15 @@
+import React, { PureComponent } from 'react';
+import logo from './logo.svg';
+import './App.css';
+
+class AppPure extends PureComponent  {
+  render() {
+    return (
+      <div>
+          Hello PureComponent!
+      </div>
+    );
+  }
+}
+
+export default AppPure;
